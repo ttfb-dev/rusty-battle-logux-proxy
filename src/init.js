4343e57d8106186f63d98c088c6eeb0018fef510
Orange <bobrov@ttfb.dev>
2021-10-12 16:57:23 +0300

    upd

--- src/init.js
+++ src/init.js
@@ -16,12 +16,8 @@ const server = new Server(
   }),
 );
 
-const clientPool = [];
-
 server.auth(async ({ client, userId, token }) => {
-  console.log('connected');
   const isAuthorized = isVkAuthorized(userId, token);
-  console.log(`isAuthorized: ${isAuthorized}`);
   return isAuthorized;
 });
 
